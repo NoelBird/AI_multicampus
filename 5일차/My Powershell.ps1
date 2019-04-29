9cf67aca8c8f8503555ab8b59fe2d77d19ab2e82
noelbird <lduldu00228@gmail.com>
2019-04-29 12:51:59 +0900

    5일차

---
+++ 5일차/My Powershell.ps1
@@ -0,0 +1,11 @@
+﻿Get-Command
+Get-Process
+Get-Help Get-Process # Power Shell 주석
+$env:PSModulePath
+$env:ALLUSERSPROFILE
+Connect-AzAccount
+Import-Module Az.Accounts
+Get-executionPolicy # PowerShell 스크립트 실행 정책(5가지 정도가 있음)
+    # Restricted: 제한됨(*.ps1 파일)
+    # Unrestricted
+Set-ExecutionPolicy Unrestricted
