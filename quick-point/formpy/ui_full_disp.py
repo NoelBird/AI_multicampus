c6c5c8299a094175407180075e1dc104af64991f
noelbird <lduldu00228@gmail.com>
2019-06-25 18:19:48 +0900

    2019-06-25 | 미니 프로젝트 시작

---
+++ quick-point/formpy/ui_full_disp.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:/Users/user/Documents/Python-programming/quick-point/ui\full_disp.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(623, 496)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(180, 230, 121, 61))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "TextLabel"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
--- quick-point/main.py
+++ quick-point/main.py
@@ -1,18 +1,104 @@
 import sys
+import os
+import markdown as md
+import pdfkit
+import imgkit
+
+
 from PyQt5 import QtWidgets
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
+from formpy.ui_mainwindow import Ui_mainWindow
+from formpy.ui_full_disp import Ui_Dialog
+# import Bokeh
+
+class Mainwin(Ui_mainWindow):
+    def __init__(self):
+        self.fname = ''
+        view = QWebEngineView()
+        view.setHtml("aaaaa")
+        # super(Mainwin, self).__init__()
+
+    def openFile(self):
+        options = QFileDialog.Options()
+        # options |= QFileDialog.DontUseNativeDialog
+        # TODO: 상속하는 부분 개선하기 None -> ?
+        fileName, _ = QFileDialog.getOpenFileName(None, "열기", "","Markdown(*.md);;Power Point(*.pptx | *.ppt);;All Files (*)", options=options)
+
+        if fileName:
+            # TODO: file에서 dot(.)이 여러개일 경우에 에러가 없는지 체크
+            ext = os.path.splitext(fileName)[1]
+            if ext == '.md':
+                self.loadMD(fileName)
+            # if ext.lower() in ['.ppt', '.pptx']:
+
+    # TODO: 특정한 디렉토리 내에서는 오픈이 안되는 이슈가 있습니다.
+    def loadMD(self, fname):
+        print(fname)
+        with open(fname, "r") as fp:
+            txt = fp.read()
+            self.txtMarkdown.setText(txt)
+
+    def saveFile(self):
+        options = QFileDialog.Options()
+        # options |= QFileDialog.DontUseNativeDialog
+        # TODO: 상속하는 부분 개선하기 None -> ?
+        fileName, _ = QFileDialog.getSaveFileName(None, "저장", "",
+                                                  "Markdown(*.md);;Power Point(*.pptx | *.ppt);;All Files (*)",
+                                                  options=options)
+
+        if fileName:
+            # TODO: file에서 dot(.)이 여러개일 경우에 에러가 없는지 체크
+            ext = os.path.splitext(fileName)[1]
+            if ext == '.md':
+                self.saveMD(fileName)
+            # if ext.lower() in ['.ppt', '.pptx']:
+
+    def saveMD(self, fname):
+        with open(fname, "wt") as fp:
+            fp.write(self.txtMarkdown.toPlainText())
+
+    def saveIMG(self, fname):
+        imgkit.from_string(md.markdown(self.txtMarkdown.toPlainText()), 'out.jpg')
+
+    # def savePDF(self, fname):
+    #     pdfkit.from_file(self.updatePreview(), 'out.pdf')
+
+    def close(self):
+        sys.exit()
+
+    def updatePreview(self):
+        try:
+            curMD = md.markdown(self.txtMarkdown.toPlainText())
+            # self.saveIMG('out.jpg')
+        except Exception as e:
+            pass
+        finally:
+            pixmap = QPixmap('out.jpg')
+            self.lblPreview.setFixedSize(self.lblPreview.size())
+            self.lblPreview.setPixmap(pixmap)
 
 
-class Form(QtWidgets.QDialog):
-    def __init__(self, parent=None):
-        QtWidgets.QDialog.__init__(self, parent)
-        self.ui = uic.loadUi("mainwindow.ui")
-        self.ui.show()
-        self.ui.previewSmall.setPixmap(QPixmap('cat04_256.png'))
+
+
+
+
 
 
 if __name__ == '__main__':
+    import sys
     app = QtWidgets.QApplication(sys.argv)
-    w = Form()
-    sys.exit(app.exec())
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Mainwin()
+    # ui = Ui_Dialog()
+    ui.setupUi(MainWindow)
+    MainWindow.setWindowIcon(QIcon('logoico.ico'))
+    web = QWebEngineView()
+    web.setUrl(QUrl("https://www.google.com"))
+    MainWindow.form_layout.addWidget(web)
+    MainWindow.show()
+    sys.exit(app.exec_())
