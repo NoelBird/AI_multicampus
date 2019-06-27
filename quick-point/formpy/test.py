88864bbc8ed867b61e1dec5d5df82ad34c470f6f
noelbird <lduldu00228@gmail.com>
2019-06-27 18:48:36 +0900

    2019-06-27 | 미니 프로젝트 발표자료 만듦

--- quick-point/formpy/test.py
+++ quick-point/formpy/test.py
@@ -1,40 +1,47 @@
-import sys
-import os
-import markdown as md
-import pdfkit
-import imgkit
-
-
 from PyQt5 import QtWidgets
 from PyQt5 import uic
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton
+from PyQt5.QtGui import QIcon, QPixmap, QClipboard
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QHBoxLayout, QDialog, QBoxLayout, QMainWindow
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, QDir, QMimeData
 from formpy.ui_mainwindow2 import Ui_mainWindow
+import PyQt5.QtCore as QtCore
 from formpy.ui_full_disp import Ui_Dialog
 
-class Mainwin:
-    def __init__(self):
-        self.fname = ''
-        view = QWebEngineView()
-        view.setHtml("aaaaa")
-        # super(Mainwin, self).__init__()
-        btn = QPushButton()
-        btn.move(200, 50)
 
-if __name__ == '__main__':
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(200, 200)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.setCentralWidget(self.centralwidget)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(50, 110, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent=parent)
+        self.setupUi(self)
+
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key_F5:
+            self.close()
+
+if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    # ui = Mainwin()
-    # ui = Ui_Dialog()
-    # ui.setupUi(MainWindow)
-    # MainWindow.setWindowIcon(QIcon('logoico.ico'))
-    # MainWindow.statusBar().showMessage('준비')
-    # web = QWebEngineView()
-    # web.setUrl(QUrl("https://www.google.com"))
-    MainWindow.layout.addWidget() #TODO: 웹뷰 추가하기
-    MainWindow.show()
+    w = MainWindow()
+    w.show()
     sys.exit(app.exec_())
--- quick-point/main.py
+++ quick-point/main.py
@@ -1,24 +1,33 @@
 import sys
 import os
 import markdown as md
+from pptx import Presentation
+from bs4 import BeautifulSoup
 
 from PyQt5 import QtWidgets
-from PyQt5 import uic
-from PyQt5.QtGui import QIcon, QPixmap, QClipboard
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QHBoxLayout, QDialog
+from PyQt5.QtGui import QIcon, QPixmap, QClipboard, QImage
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QHBoxLayout, QDialog, QBoxLayout, QMainWindow, QLabel
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl, QDir
+from PyQt5.QtCore import QUrl, QDir, QMimeData
 from formpy.ui_mainwindow2 import Ui_mainWindow
 import PyQt5.QtCore as QtCore
 from formpy.ui_full_disp import Ui_Dialog
 
 
-class Mainwin(Ui_mainWindow):
+class Mainwin(QMainWindow, Ui_mainWindow):
     def __init__(self):
         super(Mainwin, self).__init__()
+        QMainWindow.__init__(self, parent=None)
+        self.setupUi(self)
         self.fname = ''
-        self.template = ''
+        self.template = 'github.css'
+        self.keylist = []
+
+    def keyPressEvent(self, event): # clipboard to image file
+        if event.key() == QtCore.Qt.Key_F8:
+            self.imgFromClipboard()
+
 
 
     def openFile(self):
@@ -60,28 +69,110 @@ class Mainwin(Ui_mainWindow):
         with open(fname, "wt") as fp:
             fp.write(self.txtMarkdown.toPlainText())
 
+    def exportPPT(self):
+        options = QFileDialog.Options()
+        # options |= QFileDialog.DontUseNativeDialog
+        # TODO: 상속하는 부분 개선하기 None -> ?
+        fileName, _ = QFileDialog.getSaveFileName(None, "저장", "",
+                                                  "Power Point(*.pptx | *.ppt);;All Files (*)",
+                                                  options=options)
+        htmlLst = md.markdown(self.txtMarkdown.toPlainText()).split('<hr />')
+        prs = Presentation()
+        for i in range(len(htmlLst)):
+            bs = BeautifulSoup(htmlLst[i], "html.parser")
+            title_slide_layout = prs.slide_layouts[1]
+            slide = prs.slides.add_slide(title_slide_layout)
+            title = slide.shapes.title
+            subtitle = slide.placeholders[1]
+
+            # title 부분
+
+            title.text = bs.find('h1').get_text()
+            # subtitle.text = pTitle.findall(txtLst[i])[0]
+
+            # subtitle 부분
+
+            subtitle.text = bs.find('h2').get_text()
+
+            # 본문 부분
+            shapes = slide.shapes
+            body_shape = shapes.placeholders[1]
+            tf = body_shape.text_frame
+            p = tf.add_paragraph()
+            # tf.text = '\n'.join([k for k in bs.findAll('p')])
+            p.text = '안녕하세요'
+
+        prs.save(fileName)
+
+    def importPPT(self):
+        pass
+
+    def setTemplateGithub(self):
+        self.template = 'github.css'
+        self.updateWeb()
+
+    def setTemplateDracula(self):
+        self.template = 'dracula.css'
+        self.updateWeb()
+
+    def setTemplatePresentation(self):
+        self.template = 'presentation.css'
+        self.updateWeb()
+
+    def setTemplateTmp(self):
+        self.template = 'tmp.css'
+        self.updateWeb()
+
+    def imgFromClipboard(self):
+        clipboard = QApplication.clipboard()
+        mimedata = clipboard.mimeData()
+        if(mimedata.hasImage()):
+            lblImage = QLabel()
+            img = QImage(mimedata.imageData())
+            img.save('fff.png')
+            self.txtMarkdown.append("![](fff.png)")
+            # lblImage.setPixmap(mimedata.imageData())
+
+
+
+
     # def savePDF(self, fname):
     #     pdfkit.from_file(self.updatePreview(), 'out.pdf')
 
     def close(self):
         sys.exit()
 
-    def mdfullScreen(self):
-        self.showFullScreen()
+
+    def displayFullScreen(self):
+        import copy
+        wgt = QDialog()
+        wgt.showFullScreen()
+        layout = QBoxLayout(QBoxLayout.TopToBottom)
+        wgt.setLayout(layout)
+        layout.addWidget(self.wgtWeb) # widget이 새 창꺼랑 같이 꺼지는 경향이 있음
+        # wgt.setWindowState()
+        wgt.exec_()
+
+
+
+    # def keyPressEvent(self, event):
+    #     if event.key() == QtCore.Qt.Key_Control | QtCore.Qt.Key_V:
+    #         print("here")
+    #     event.accept()
 
     def updateWeb(self):
         try:
             curMD = md.markdown(self.txtMarkdown.toPlainText())
             baseDir = "C:\\Users\\user\\Documents\\Python-programming\\quick-point"
-            css = "<link rel=\"stylesheet\" type=\"text/css\" href=\"templates/template1.css\"><br>"
+            cssFile = self.template
+            css = "<link rel=\"stylesheet\" type=\"text/css\" href=\"templates/%s\"><br>" % cssFile
             totalHtml = "<div class='markdown-body'>" + css + '\n' + curMD +"</div>"
             with open("curHtml.html", "wt") as fp:
                 fp.write(totalHtml)
             print(css+curMD)
             # self.wgtWeb.setHtml(css+curMD)
-            print(QDir.currentPath() + '/curHtml.html')
+            # print(QDir.currentPath() + '/curHtml.html')
             self.wgtWeb.setUrl(QUrl.fromLocalFile(QDir.currentPath() + '/curHtml.html'))
-
         except Exception as e:
             pass
         finally:
@@ -109,31 +200,12 @@ class Mainwin(Ui_mainWindow):
 #         ctrlWin += 1
 #         # submitFrame = self.subwindow.findChild(QtGui.QFrame, "frameSubmit")
 
-def showFullScreen(mainwin):
-    pass
-    wgt = QDialog()
-    wgt.showFullScreen()
-    # wgt.setWindowFlags(QtCore.Qt.Window | )
-    wgt.showFullScreen()
-    wgt.
-    wgt.exec_()
 
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    # wmain = ctrlWin()
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Mainwin()
-    ui.setupUi(MainWindow)
+    MainWindow = Mainwin()
     MainWindow.setWindowIcon(QIcon('logoico.ico'))
     MainWindow.statusBar().showMessage('준비')
-    # web = QWebEngineView()
-    # web.setUrl(QUrl("https://www.google.com"))
-    # web.setHtml("aaaaa")
-    # MainWindow.findChild(QHBoxLayout).addWidget(web) #TODO: 웹뷰 추가하기
-    # MainWindow.findChild(QWebEngineView).setHtml('aaaa')
-    # web.move()
-    MainWindow.show()  # 창 화면으로 화면 띄우기
-    # MainWindow.showFullScreen()  # 전체화면으로 화면을 띄우기
-    showFullScreen(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
